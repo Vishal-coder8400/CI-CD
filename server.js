cd842daebc9722d5accdf5e0be4b075c57520b80
vishal tiwari <vktgkp7@gmail.com>
2026-05-06 10:38:13 +0530

    first commit

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-res.send("Version 2 Auto Deploy 🔥");
+  res.send("Broken App 😵");
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+// ❌ BROKEN VARIABLE
+app.listen(PORTTT, () => {
+  console.log(`Server running`);
 });
