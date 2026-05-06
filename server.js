3692799ffe4e501b5b0f135bae0305953982f48c
vishal tiwari <vktgkp7@gmail.com>
2026-05-06 10:48:39 +0530

    first commit

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 // ❌ WRONG VARIABLE
-app.listen(PORTTT, () => {
+app.listen(PORT, () => {
   console.log(`Server running`);
 });
